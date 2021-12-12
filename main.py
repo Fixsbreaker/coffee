f3dcfe861be268b010244cb578c6685ebb3a5c2e
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2021-12-12 13:55:14 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5 import uic
+import sqlite3
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("cof.ui", self)
+        self.con = sqlite3.connect('coffee.db')
+        self.pushButton.clicked.connect(self.do)
+
+    def do(self):
+        req = 'SELECT * FROM cofee'
+        print(req)
+        cur = self.con.cursor()
+        res = cur.execute(req).fetchall()
+        self.tableWidget.setRowCount(len(res))
+        self.tableWidget.setColumnCount(len(res[0]))
+        for i, elem in enumerate(res):
+            for j, value in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(value)))
+        self.tableWidget.resizeColumnsToContents()
+        self.tableWidget.setHorizontalHeaderLabels(['ID:', 'Название:',
+                                                    "Обжарка:", "Тип:",
+                                                    "Вкус:", "Цена:",
+                                                    "Объем:"])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
